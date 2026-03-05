03309b07df532b54ff32f7e71ccb2d3a29b8332f
stinkysock07 <stinkysockaccount@protonmail.com>
2026-03-05 01:11:54 -0500

    updating middleware security procedures

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -3,9 +3,34 @@ import { Core } from '@strapi/strapi';
 export default ({ env }: { env: any }) => [
   'strapi::logger',
   'strapi::errors',
- 'strapi::security',
- {
-  name: 'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            env('R2_DEV_DOMAIN'),
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            env('R2_DEV_DOMAIN'),
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
     config: {
       origin: [env('FRONTEND_URL'), 'http://localhost:3000'], 
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
