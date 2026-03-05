664fc2ceb821e70e48b6f5a222e9b1f3b7321528
stinkysock07 <stinkysockaccount@protonmail.com>
2026-03-05 01:56:58 -0500

    Removing comments in plugin, this commit marks the bucket, backend image corruption bug. using a cloudflare r2 bucket, im storing image data in a bucket for easier and quicker data storage and retrevial. this also removes ephermal images, now all images will stay in tact on redeploys.

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -8,14 +8,13 @@ export default ({ env }) => ({
             accessKeyId: env('AWS_ACCESS_KEY_ID'),
             secretAccessKey: env('AWS_SECRET_ACCESS_KEY'),
           },
-          region: 'auto', // R2 requires 'auto'
+          region: 'auto', 
           endpoint: env('CF_R2_ENDPOINT'), 
           forcePathStyle: true, 
         },
         params: {
           Bucket: env('CF_BUCKET_NAME'),
         },
-        // The official provider uses 'baseUrl' to replace 'undefined' with your R2 link
         baseUrl: env('R2_DEV_DOMAIN', 'https://pub-da902343a39f42e2b8a99078b67aa4b4.r2.dev'),
       },
     },
