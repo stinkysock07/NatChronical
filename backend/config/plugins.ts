5bc35ed7696947a99cb5fa90cf0d5babd38f54da
stinkysock07 <stinkysockaccount@protonmail.com>
2026-03-05 01:38:23 -0500

    fixing packages and changing to public proxy

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -10,8 +10,8 @@ export default ({ env }) => ({
         params: {
           Bucket: env('CF_BUCKET_NAME'),
         },
-        baseUrl: env('R2_DEV_DOMAIN', 'https://pub-da902343a39f42e2b8a99078b67aa4b4.r2.dev'),
-        publicUrl: env('R2_DEV_DOMAIN', 'https://pub-da902343a39f42e2b8a99078b67aa4b4.r2.dev'),
+        // This specific package uses this key name:
+        cloudflarePublicProxy: env('R2_DEV_DOMAIN', 'https://pub-da902343a39f42e2b8a99078b67aa4b4.r2.dev'),
       },
     },
   },
