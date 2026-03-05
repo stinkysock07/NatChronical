e4f16b7cba3e6dec85f7bf7c54ab4fddcdca58dc
stinkysock07 <stinkysockaccount@protonmail.com>
2026-03-05 16:04:23 -0500

    removing double resend intializing

--- frontend/app/api/send/route.ts
+++ frontend/app/api/send/route.ts
@@ -1,11 +1,9 @@
 import { Resend } from 'resend';
 import { NextResponse } from 'next/server';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 export async function POST(req: Request) {
     const resend = new Resend(process.env.RESEND_API_KEY);
-    
+
   try {
     const { turnstileToken, ...body } = await req.json();
     const { name, contact_email, subject, tip_description } = body;
