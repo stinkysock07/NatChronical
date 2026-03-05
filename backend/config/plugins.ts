81b51ac7d6c2d64c4b5151abfdca92d360386e34
stinkysock07 <stinkysockaccount@protonmail.com>
2026-03-05 00:11:44 -0500

    adding versioning to stop corruption

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -11,15 +11,10 @@ const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin =>
           region: 'auto',
           bucket: process.env.CF_BUCKET_NAME,
           endpoint: process.env.CF_R2_ENDPOINT,
+          signatureVersion: 's3v4',
         },
         params: {
           Bucket: process.env.CF_BUCKET_NAME,
-        },
-      },
-      security: {
-        contentTypes: {
-          allowedTypes: ['image/jpeg', 'image/png', 'image/gif', 'image/webp'],
-          maxFileSize: 52428800, // 50MB
         },
       },
     },
