d5989470f93265391f5c61f13dd9eee383261440
stinkysock07 <stinkysockaccount@protonmail.com>
2026-04-07 14:32:12 -0400

    changing back to orignal fetching style

--- backend/src/api/tweets/routes/tweets.ts
+++ backend/src/api/tweets/routes/tweets.ts
@@ -5,12 +5,16 @@ export default {
         {
             method: 'GET',
             path: '/',
-            handler: 'tweets.find',
-        },
-        {
-            method: 'GET',
-            path: '/:id',
-            handler: 'tweets.findOne',
-        },
-    ],
-} as unknown as Core.Route;
+            handler: async (ctx: any) => {
+                try {
+                    const response = await strapi.service('api::tweets.tweets').getTweets();
+                    ctx.body = { data: response.data };
+                } catch (error: any) {
+                    console.error('Tweets error:', error);
+                    ctx.status = 500;
+                    ctx.body = { error: error.message };
+                }
+            }
+        }
+    ]
+};
