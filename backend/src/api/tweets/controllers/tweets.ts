cdba4e09a97e2d05cee31c520f42dfd3d9fffd05
stinkysock07 <stinkysockaccount@protonmail.com>
2026-04-07 14:08:42 -0400

    Add find and findOne controller methods for tweets

--- backend/src/api/tweets/controllers/tweets.ts
+++ backend/src/api/tweets/controllers/tweets.ts
@@ -1,6 +1,13 @@
 export default {
-  async list(ctx) {
+  async find(ctx) {
     const response = await strapi.service('api::tweets.tweets').getTweets();
     ctx.body = { data: response.data };
+  },
+
+  async findOne(ctx) {
+    const { id } = ctx.params;
+    const response = await strapi.service('api::tweets.tweets').getTweets();
+    const tweet = response.data.find((t: any) => t.id === id);
+    ctx.body = { data: tweet || null };
   }
 };
